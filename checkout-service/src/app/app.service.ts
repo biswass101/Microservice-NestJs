02d87486449372c21640ceca3d3115d9f33acfbb
biswass101 <biswassnaeemcse@gmail.com>
2026-05-19 14:07:17 +0600

    feat(consul): add service discovery with consul

--- checkout-service/src/app/app.service.ts
+++ checkout-service/src/app/app.service.ts
@@ -1,8 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import Consul from "consul";
 
 @Injectable()
+
 export class AppService {
-  getData(): { message: string } {
-    return { message: 'Hello API' };
+  private consul = new Consul({ host: 'localhost', port: 8500 });
+
+  async discoverAndCallPayment() {
+    const services = await this.consul.agent.service.list();
+
+    const paymentInfo = services['payment-unique-id-1'];
+
+    if(!paymentInfo) {
+      throw new Error("Payment service not found in registry");
+    }
+
+    const address = 'localhost';
+    const port = paymentInfo.Port;
+
+    const finalUrl = `http://${address}:${port}/api/health`;
+
+    console.log(`Discovery successful - Calling payment service at ${finalUrl}`);
+
+    return { message: 'Service discovery successful',
+      discoveredUrl: finalUrl,
+      serviceData: paymentInfo
+    }
   }
 }
--- payment-service/src/main.ts
+++ payment-service/src/main.ts
@@ -8,5 +8,30 @@ async function bootstrap() {
   const port = 3005;
   const consul = new Consul({ host: 'localhost', port: 8500});
 
+  const serviceId = 'payment-unique-id-1';
+
+  const registrationDetails = {
+    name: "payment-service",
+    address : "host.docker.internal",
+    port: port,
+    id: serviceId,
+    check: {
+      name: "payment-service-health",
+      http: `http://host.docker.internal:${port}/api/health`,
+      interval: "10s",
+      timeout: "5s",
+    }
+  };
   
+  await consul.agent.service.register(registrationDetails);
+
+  process.on("SIGINT", async () => {
+    await consul.agent.service.deregister(serviceId);
+    process.exit();
+  });
+
+  await app.listen(port);
+  console.log(`Payment service is running on port ${port} and registered with Consul`);
 }
+
+bootstrap();
